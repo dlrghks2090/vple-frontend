0e2af3c95bb5aecaa0f4aa31dd2e8ebf093aa67b
Kim IkHwan <dlrghks2090@gmail.com>
2025-03-29 02:39:22 +0900

    Feat: kakao login 연결 완료

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -7,8 +7,9 @@ import {
     WrapLogin,
 } from './LoginStyle';
 
-const CLIENT_ID = '9bace5ab316d4073d7412f542dd4f2c8';
-const REDIRECT_URI = `http://${window.location.host}/user/kakao/callback`;
+const CLIENT_ID = '5681ba5beb345aa85d32837edd41bde8';
+// const REDIRECT_URI = `http://${window.location.host}/user/kakao/callback`;
+const REDIRECT_URI = `http://localhost:3000/user/kakao/callback`;
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`
 
 export default function Login() {
